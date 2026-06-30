9838cd49f49356b83cee05616ac025b04b328a88
PvtMilo <flip360.id.solo@gmail.com>
2026-06-30 16:12:25 +0700

    (ui) change button color welcome exp

--- app/pages/category/love-and-guarded/index.vue
+++ app/pages/category/love-and-guarded/index.vue
@@ -2,7 +2,7 @@
   <div class="wrapper welcome-love-and-guarded">
       <NextButton
         to="/category/love-and-guarded/explanation-1"
-        button-class="long-btn-welcome bg-fourth-blue text-white"
+        button-class="long-btn-welcome bg-fourth-blue text-tertiary-btn"
         >Lanjut</NextButton
       >
   </div>
--- app/pages/category/taking-the-step/index.vue
+++ app/pages/category/taking-the-step/index.vue
@@ -2,7 +2,7 @@
   <div class="wrapper welcome-pre-explanation taking-the-step">
       <NextButton
         to="/category/taking-the-step/explanation-1"
-        button-class="long-btn-welcome bg-tertiary-pink text-white"
+        button-class="long-btn-welcome bg-tertiary-pink text-secondary-btn"
         >Lanjut</NextButton
       >
   </div>
